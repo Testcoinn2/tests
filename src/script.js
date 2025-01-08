61060bdfea4e34444779026e650006fdc63ed7b9
Testcoinn2 <lletechnn@gmail.com>
2025-01-08 11:10:29 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function Positivememen(number) {
+      // if number is less than zero multiply with -1, otherwise returns as it is
+      return number < 0 ? number * -1 : number;
+   };
+
+// If a number you enter is positive, return the same result; if a negative number is entered, multiply by -1 and get the output as positive.
+console.log(Positivememen(-78));
+
+// output 78
